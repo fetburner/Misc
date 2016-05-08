af736dcf194be1b2106cc4058e1bb51a6d92a21b
fetburner <masayuki2718@yahoo.co.jp>
2016-05-08 21:01:10 +0900

    Implemented fresh variable generation

--- STLC/Id.v
+++ STLC/Id.v
@@ -1,6 +1,26 @@
 Require Import Arith MSets.
 
-Module FSet := MSetList.Make (Nat_as_OT).
+Module FSet.
+  Include (MSetList.Make (Nat_as_OT)).
+
+  Definition fresh s :=
+    match max_elt s with
+    | None => 0
+    | Some n => S n
+    end.
+
+  Lemma fresh_spec : forall s, ~ In (fresh s) s.
+  Proof.
+    unfold fresh.
+    intros s ?.
+    remember (max_elt s) as m.
+    symmetry in Heqm.
+    destruct m.
+    - eapply max_elt_spec2; eauto.
+    - apply max_elt_spec3 in Heqm.
+      destruct (Heqm _ H).
+  Qed.
+End FSet.
 Module FSetProperties := WProperties (FSet).
 
 Definition t := FSet.E.t.
