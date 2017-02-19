edf9b893abb307ec81f505898a67f33f4d313208
fetburner <masayuki2718@yahoo.co.jp>
2017-02-20 05:52:39 +0900

    Meld proof of Newman's lemma into ARS.v

--- ARS.v
+++ ARS.v
@@ -37,6 +37,11 @@ Section ARS.
     R x y1 ->
     clos_refl_trans _ R x y2 ->
     joinable y1 y2.
+  Definition locally_confluent :=
+    forall a1 a2 a3,
+    R a1 a2 ->
+    R a1 a3 ->
+    joinable a2 a3.
 
   Definition terminating :=
     well_founded (fun x y => R y x).
@@ -328,6 +333,33 @@ Section ARS.
         intros ? ?.
         eapply Hbounded'.
         eauto.
+  Qed.
+
+  Lemma Newman :
+    terminating ->
+    locally_confluent ->
+    confluent.
+  Proof.
+    Local Hint Constructors clos_refl_trans_1n.
+    Local Hint Resolve clos_rt1n_rt.
+    Local Hint Resolve clos_rt_rt1n.
+    intros HSN HLC a1 a2 a3 HRs12 HRs13.
+    unfold joinable.
+    apply clos_rt_rt1n in HRs12.
+    apply clos_rt_rt1n in HRs13.
+    generalize dependent a3.
+    generalize dependent a2.
+    induction a1 as [a1 IHa1] using (well_founded_induction HSN).
+    intros ? HRs12 ? HRs13.
+    inversion HRs12 as [| a1' ? HR11' HRs1'2 ];
+      inversion HRs13 as [| a1'' ? HR11'' HRs1''3 ]; subst;
+        eauto.
+    destruct (HLC _ _ _ HR11' HR11'') as [? [HRs1'4]].
+    apply clos_rt_rt1n in HRs1'4.
+    destruct (IHa1 _ HR11' _ HRs1'2 _ HRs1'4) as [a5 []].
+    assert (HRs1''5 : clos_refl_trans_1n _ R a1'' a5) by eauto.
+    destruct (IHa1 _ HR11'' _ HRs1''3 _ HRs1''5) as [? []].
+    eauto.
   Qed.
 End ARS.
 
