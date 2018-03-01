9c9c25211be7d333cfb93d481bdaaf467be081b0
fetburner <masayuki2718@yahoo.co.jp>
2018-03-01 20:20:06 +0900

    Refactoring

--- Ramanujan.hs
+++ Ramanujan.hs
@@ -1,22 +1,22 @@
 {-# LANGUAGE Strict #-}
 {-# LANGUAGE StrictData #-}
 
+binarySplitting p q r i 0 = (p i, q i * r i, r i)
+binarySplitting p q r i n =
+  let
+     (a,  b,  c)  = binarySplitting p q r (2 * i - 1) (n - 1)
+     (a', b', c') = binarySplitting p q r (2 * i) (n - 1)
+  in (a * a', a * b' + b * c', c * c')
+
 main :: IO ()
 main = do
   s <- getLine
   let
     n = read s
-    (_, x, y) = go 1 $ max 0 $ ceiling $
-      logBase 2 (fromIntegral n) - 1.55849716603186539
+    (_, x, y) =
+      binarySplitting
+        (\i -> (1 - 4 * i) * (2 * i - 1) * (4 * i - 3))
+        (\i -> (21460 * i - 20337))
+        (\i -> 24893568 * i * i * i) 1 $
+      max 0 $ ceiling $ logBase 2 (fromIntegral n) - 1.55849716603186539
   putStrLn $ show $ 3528 * y * (10 ^ n) `div` x
-
-  where
-    go i 0 =
-      ( (1 - 4 * i) * (2 * i - 1) * (4 * i - 3)
-      , 24893568 * i * i * i * (21460 * i - 20337)
-      , 24893568 * i * i * i )
-    go i n =
-      let
-        (a,  b,  c)  = go (2 * i - 1) (n - 1) 
-        (a', b', c') = go (2 * i) (n - 1)
-      in (a * a', a * b' + b * c', c * c')
