5d6c0f72cc2bebf4e20fc5f0f27abf9d6df3f88d
fetburner <masayuki2718@yahoo.co.jp>
2019-01-28 23:43:55 +0900

    Improve the code for Ramanujan's series

--- Ramanujan.hs
+++ Ramanujan.hs
@@ -1,22 +1,25 @@
 {-# LANGUAGE Strict #-}
 {-# LANGUAGE StrictData #-}
 
-binarySplitting p q r i 0 = (p i, q i * r i, r i)
-binarySplitting p q r i n =
-  let
-     (a,  b,  c)  = binarySplitting p q r (2 * i - 1) (n - 1)
-     (a', b', c') = binarySplitting p q r (2 * i) (n - 1)
-  in (a * a', a * b' + b * c', c * c')
-
 main :: IO ()
 main = do
   s <- getLine
   let
     n = read s
     (_, x, y) =
-      binarySplitting
-        (\i -> (1 - 4 * i) * (2 * i - 1) * (4 * i - 3))
-        (\i -> (21460 * i - 20337))
-        (\i -> 24893568 * i * i * i) 1 $
-      max 0 $ ceiling $ logBase 2 (fromIntegral n) - 1.55849716603186539
+      binarySplitting 0 $
+      max 1 $ ceiling $ fromIntegral n * 0.1697522772858307
   putStrLn $ show $ 3528 * y * (10 ^ n) `div` x
+  where
+    p i = (1 - 4 * i) * (2 * i - 1) * (4 * i - 3)
+    q i = 21460 * i - 20337
+    r i = 24893568 * i * i * i
+
+    -- (n, m]
+    binarySplitting n m
+      | m - n <= 1 = (p m, q m * r m, r m)
+      | otherwise  =
+          let
+             (a,  b,  c)  = binarySplitting n ((n + m) `div` 2)
+             (a', b', c') = binarySplitting ((n + m) `div` 2) m
+          in (a * a', a * b' + b * c', c * c')
